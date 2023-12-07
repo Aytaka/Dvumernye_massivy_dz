a4d2693c7f8ab21b9689cf53475c739b7f5d8d24
Ayta <ayta88@list.ru>
2023-12-07 22:10:52 +0300

    work3

--- Program.cs
+++ Program.cs
@@ -52,56 +52,12 @@
 //Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку 
 //массива.
 
-void InputMatrix(int[,] matrix)
-{
-    for(int i = 0; i < matrix.GetLength(0); i++)
-        for(int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(-10, 11);
-}
-
-void PrintMatrix(int[,] matrix)
-{
-    for(int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for(int j = 0; j < matrix.GetLength(1); j++)
-            Console.Write(matrix[i, j] + "\t");
-        Console.WriteLine();
-    }
-}
-
-void ChangeArray(int[,] matrix)
-{
-    
-    int temporary = 0;
-    for (int i=0; i < matrix.GetLength(1); i++)
-    {
-        temporary = matrix[0, i];
-        matrix[0, i] = matrix[(matrix.GetLength(0) - 1), i];
-        matrix[(matrix.GetLength(0) - 1), i] = temporary;
-     }
-}
-
-
-Console.Clear();
-Console.Write("Введите размеры матрицы: ");
-int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0], size[1]];
-InputMatrix(matrix);
-PrintMatrix(matrix);
-Console.WriteLine();
-ChangeArray(matrix);
-PrintMatrix(matrix);
-
-//Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с 
-//наименьшей суммой элементов.
-
 // void InputMatrix(int[,] matrix)
 // {
 //     for(int i = 0; i < matrix.GetLength(0); i++)
 //         for(int j = 0; j < matrix.GetLength(1); j++)
-//             matrix[i, j] = new Random().Next(0, 11);
+//             matrix[i, j] = new Random().Next(-10, 11);
 // }
-
 
 // void PrintMatrix(int[,] matrix)
 // {
@@ -113,29 +69,79 @@ PrintMatrix(matrix);
 //     }
 // }
 
-
-// void GetResult(int[] matrix)
+// void ChangeArray(int[,] matrix)
 // {
-//     int position = 0;
-//     int min = matrix[0];
-//     for (int i = 1; i < matrix.Length; i++)
+    
+//     int temporary = 0;
+//     for (int i=0; i < matrix.GetLength(1); i++)
 //     {
-//         if (matrix[i] < min)
-//         {
-//             min = matrix[i];
-//             position = i;
-//         }
-//     }
-//     if(position == 1) Console.WriteLine($"\nНаименьшая сумма элементов, равная {min}, находится во {position+1}й строке");
-//     else Console.WriteLine($"\nНаименьшая сумма элементов, равная {min}, находится в {position+1}й строке");
+//         temporary = matrix[0, i];
+//         matrix[0, i] = matrix[(matrix.GetLength(0) - 1), i];
+//         matrix[(matrix.GetLength(0) - 1), i] = temporary;
+//      }
 // }
+
 
 // Console.Clear();
 // Console.Write("Введите размеры матрицы: ");
 // int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 // int[,] matrix = new int[size[0], size[1]];
-// int[] array = new int[size[0]];
 // InputMatrix(matrix);
 // PrintMatrix(matrix);
-// GetResult(matrix);
-// Console.WriteLine(string.Join(", ", array));
+// Console.WriteLine();
+// ChangeArray(matrix);
+// PrintMatrix(matrix);
+
+//Задача 3: Задайте  двумерный массив. Напишите программу, которая будет находить строку с 
+//наименьшей суммой элементов.
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 11);
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+
+void GetResult(int[,] matrix)
+{
+    int position = 0;
+    int min = 0;
+    int sum = 0;
+    int[] array = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        array[i] = sum;
+    }
+    Console.WriteLine(string.Join(", ", array));
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == array.Min()) Console.WriteLine($"\nНаименьшая сумма элементов, находится в {i+1}й строке");
+    }
+}
+
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+GetResult(matrix);
